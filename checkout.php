6c3558ba5b2c06174f36b97b679c1b4623239cd1
JonathanMorales2 <141509064+JonathanMorales2@users.noreply.github.com>
2023-08-06 22:20:53 +0200

    Update checkout.php

--- checkout.php
+++ checkout.php
@@ -1,162 +1,46 @@
-<!DOCTYPE html>
-<html>
-<head>
-<meta name="viewport" content="width=device-width, initial-scale=1">
-<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-<style>
-body {
-  font-family: Arial;
-  font-size: 17px;
-  padding: 8px;
+<?php
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    // Collect payment data from the form
+    $amount = $_POST['amount'];
+    $currency = $_POST['currency'];
+    // ... Collect other relevant data from the form
+
+    // Set up API endpoint and headers
+    $endpoint = 'https://api.sandbox.checkout.com/tokens'; 
+    $headers = [
+        'Authorization: Basic ' . base64_encode('k_sbox_fzspyszrkddxsgozkyqjbw4w7aw:sk_sbox_o2nulev2arguvyf6w7sc5fkznas'),
+        'Content-Type: application/json',
+    ];
+
+    // Create payment data array
+    $paymentData = [
+        'channel_id' => 'pc_zs5fqhybzc2e3jmq3efvybybpq',
+        'amount' => $amount,
+        'currency' => $currency,
+        // ... Add other payment-related data
+    ];
+
+    // Send payment request to the payment provider's API
+    $ch = curl_init($endpoint);
+    curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'POST');
+    curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($paymentData));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+    curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    // Process the response from the payment provider
+    $responseData = json_decode($response, true);
+
+    if ($responseData['status'] === 'success') {
+        // Payment was successful
+        // ... Handle successful payment
+    } else {
+        // Payment failed
+        // ... Handle payment failure
+    }
 }
+?>
 
 
-* {
-  box-sizing: border-box;
-}
-
-.row {
-  display: -ms-flexbox; /* IE10 */
-  display: flex;
-  -ms-flex-wrap: wrap; /* IE10 */
-  flex-wrap: wrap;
-  margin: 0 -16px;
-}
-
-.col-25 {
-  -ms-flex: 25%; /* IE10 */
-  flex: 25%;
-}
-
-.col-50 {
-  -ms-flex: 50%; /* IE10 */
-  flex: 50%;
-}
-
-.col-75 {
-  -ms-flex: 75%; /* IE10 */
-  flex: 75%;
-}
-
-.col-25,
-.col-50,
-.col-75 {
-  padding: 0 16px;
-}
-
-.container {
-  background-color: ##f2f2f2;
-  padding: 5px 20px 15px 20px;
-  border: 1px solid lightgrey;
-  border-radius: 3px;
-}
-
-input[type=text] {
-  width: 100%;
-  margin-bottom: 20px;
-  padding: 12px;
-  border: 1px solid #ccc;
-  border-radius: 3px;
-}
-
-label {
-  margin-bottom: 10px;
-  display: block;
-}
-
-.icon-container {
-  margin-bottom: 20px;
-  padding: 7px 0;
-  font-size: 24px;
-}
-
-.btn {
-  background-color: #8C9E6E;
-  color: white;
-  padding: 12px;
-  margin: 10px 0;
-  border: none;
-  width: 100%;
-  border-radius: 3px;
-  cursor: pointer;
-  font-size: 17px;
-}
-
-.btn:hover {
-  background-color: #45a049;
-}
-
-a {
-  color: #2196F3;
-}
-
-hr {
-  border: 1px solid lightgrey;
-}
-
-span.price {
-  float: right;
-  color: grey;
-}
-
-/* Responsive layout - when the screen is less than 800px wide, make the two columns stack on top of each other instead of next to each other (and change the direction - make the "cart" column go on top) */
-@media  (max-width: 800px) {
-  .row {
-    flex-direction: column-reverse;
-  }
-  .col-25 {
-    margin-bottom: 20px;
-  }
-}
-</style>
-</head>
-<body>
-
-<h2>T-Shirt Store</h2>
-<div class="row">
-  <div class="col-75">
-    <div class="container">
-      <form action="/action_page.php">
-      <div class="row">
-
-
-<div class="col-50">
-            <h3>Payment</h3>
-            <label for="fname">Accepted Cards</label>
-            <div class="icon-container">
-              <i class="fa fa-cc-visa" style="color:navy;"></i>
-              <i class="fa fa-cc-amex" style="color:blue;"></i>
-              <i class="fa fa-cc-mastercard" style="color:black;"></i>
-            </div>
-            <label for="ccnum">Credit card number</label>
-            <input type="text" id="ccnum" name="cardnumber" placeholder="1111 2222 3333 4444">
-            <label for="expdate">Exp Date</label>
-            <input type="text" id="expdate" name="expdate" placeholder="01/2024">
-            <div class="row">
-              <div class="col-50">
-              </div>
-              <div class="col-50">
-                <label for="cvv">CVV</label>
-                <input type="text" id="cvv" name="cvv" placeholder="123">
-              </div>
-            </div>
-          </div>
-          
-            </div>
-          
-        <input type="submit" value="Continue to checkout" class="btn">
-      </form>
-    </div>
-  </div>
-  <div class="col-25">
-    <div class="container">
-      <h4>Cart <span class="price" style="color:black"><i class="fa fa-shopping-cart"></i> <b>1</b></span></h4>
-      <p><a href="#">Product 1</a> <span class="price">20€</span></p>
-      <hr>
-      <p>Total <span class="price" style="color:black"><b>20€</b></span></p>
-    </div>
-  </div>
-</div>
-
-</body>
-</html>
